0c9ed345f6292189f9d7820f33885933d68e9068
Thomas Maxey <tmaxey201@gmail.com>
2021-04-18 18:58:15 -0400

    created basic HTML and a Javascript countdown timer

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+
+var startButton = document.getElementById("startButton")
+var timer = document.getElementById("timer")
+var secondsLeft = 75;
+
+function setTime() {
+    timerInterval = setInterval(function() {
+        secondsLeft--;
+        timer.textContent= "Time:" + secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000)
+}
+
+startButton.addEventListener('click', setTime);
+
