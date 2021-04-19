32051bb6f20cd6c74d1d68e41d4e9f12cde889e6
Thomas Maxey <tmaxey201@gmail.com>
2021-04-19 14:30:45 -0400

    added some CSS, HTML changes and working on the start page

---
+++ assets/questions.js
@@ -0,0 +1,60 @@
+//created an array for the numbers, questions, options, and answers
+var questions = [
+    {
+        numb: 1,
+        question: "Commonly used Data Types do not include:",
+        answer: "Alerts"
+        options: [
+            "Strings",
+            "Booleans",
+            "Alerts",
+            "Numbers",
+        ]
+    },
+    {
+        numb: 2,
+        question: "The condition in an if/else statement is enclosed within _______.",
+        answer: "Parenthesis"
+        options: [
+            "Quotes",
+            "Curly brackets",
+            "Parenthesis",
+            "Square brackets",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Arrays in JavaScript can be used to store _____.",
+        answer: "All the above"
+        options: [
+            "Numbers and strings",
+            "Other arrays",
+            "Booleans",
+            "All the above",
+        ]
+    },
+    {
+        numb: 4,
+        question: "String values must be enclosed within _______",
+        answer: "Quotes"
+        options: [
+            "Commas",
+            "Curly brackets",
+            "Quotes",
+            "Parenthesis",
+        ]
+    },
+    {
+        numb: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "Console.log"
+        options: [
+            "JavaScript",
+            "Terminal/bash",
+            "For loops",
+            "Console.log",
+        ]
+    },
+
+
+];
