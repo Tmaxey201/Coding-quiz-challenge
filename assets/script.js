ed7ef9dba20a60b4bf39d88bb20ed38d36479bdc
Thomas Maxey <tmaxey201@gmail.com>
2021-04-20 12:58:27 -0400

    created a script to hide start page when button is clicked

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 
-var startButton = document.getElementById("startButton")
-var timer = document.getElementById("timer")
+var startButton = document.getElementById("startButton");
+var timer = document.getElementById("timer");
 var secondsLeft = 75;
 
 function setTime() {
@@ -17,9 +17,29 @@ function setTime() {
 startButton.addEventListener('click', setTime);
 
 
+// hide start page
+
+var button = document.getElementById('startButton');
+
+button.onclick = function() {
+    var div = document.getElementById('startBox');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};
+
+// show quiz page 
+
+// button.onclick = function() {
+//     var div = document.getElementById('quiz');
+//     if (div.style.display == 'none') {
+//         div.style.display == "block";
+//     }
+// }
+
 
 //pulling question,option,answers from the array
 
-function showQuestions(){
-    var que_text = document.querySelector("");
-}
