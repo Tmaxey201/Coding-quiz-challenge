e080b14a020c1ae727b3e8a8f5538f50447f9065
Thomas Maxey <tmaxey201@gmail.com>
2021-04-21 13:19:26 -0400

    created a function to pull from my array of question and answer options

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,69 @@ var startButton = document.getElementById("startButton");
 var timer = document.getElementById("timer");
 var secondsLeft = 75;
 
+var questions = [
+    {
+        numb: 1,
+        question: "Commonly used Data Types do not include:",
+        answer: "Alerts",
+        options: [
+            "Strings",
+            "Booleans",
+            "Alerts",
+            "Numbers",
+        ]
+    },
+    {
+        numb: 2,
+        question: "The condition in an if/else statement is enclosed within _______.",
+        answer: "Parenthesis",
+        options: [
+            "Quotes",
+            "Curly brackets",
+            "Parenthesis",
+            "Square brackets",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Arrays in JavaScript can be used to store _____.",
+        answer: "All the above",
+        options: [
+            "Numbers and strings",
+            "Other arrays",
+            "Booleans",
+            "All the above",
+        ]
+    },
+    {
+        numb: 4,
+        question: "String values must be enclosed within _______",
+        answer: "Quotes",
+        options: [
+            "Commas",
+            "Curly brackets",
+            "Quotes",
+            "Parenthesis",
+        ]
+    },
+    {
+        numb: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "Console.log",
+        options: [
+            "JavaScript",
+            "Terminal/bash",
+            "For loops",
+            "Console.log",
+        ]
+    },
+
+
+];
+
+var counter = 1 
+var questions
+
 function setTime() {
     timerInterval = setInterval(function() {
         secondsLeft--;
@@ -25,22 +88,23 @@ var button = document.getElementById('startButton');
 button.onclick = function() {
     document.getElementById('startBox').style.display = "none";
     document.getElementById('quiz').style.display = "block";
+    showQuestions(0)
 }
 
-
+let que_count = 0
 
 
 //pulling question,option,answers from the array
 
-var  lastquestionIndex = questions.legnth - 1
-let runningQuestionIndex = 0
 
-function showQuestion() {
-    let q = questions[runningQuestionIndex]
-    questions.innerHTML = "<p>" +q.question+  "</p>"
-    choiceA.innerHTML = q.choiceA
-    choiceB.innerHTML = q.choiceB
-    choiceC.innerHTML = q.choiceC
-    choiceD.innerHTML = q.choiceD
-
+function showQuestions(index){
+    var que_text = document.querySelector(".que_text");
+    var option_list = document.querySelector(".option_list");
+    var que_tag = '<span>' + questions[index].question + '</span>';
+    var option_tag = '<div class="buttons">' + questions[index].options[0] + '<span></span></div>'
+                    + '<div class="buttons">' + questions[index].options[1] + '<span></span></div>'
+                    + '<div class="buttons">' + questions[index].options[2] + '<span></span></div>'
+                    + '<div class="buttons">' + questions[index].options[3] + '<span></span></div>'
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
 }
